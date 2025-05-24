17f2abb1eae8aae35bbb4fd0f6cb5e044df44401
EkaPutri <adifa.note2@gmail.com>
2025-05-24 14:48:58 +0800

    fix bluetooth escpos printer dependency

--- app/screens/setting/printer.tsx
+++ app/screens/setting/printer.tsx
@@ -3,7 +3,7 @@ import { View, Text } from "react-native";
 export default function Printer() {
   return (
     <View>
-      <Text>Halaman Akun</Text>
+      <Text>Pengaturan Printer</Text>
     </View>
   );
 }
